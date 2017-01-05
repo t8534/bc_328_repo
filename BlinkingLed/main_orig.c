d37227f78a8f6eab880a276d6956ff289ef3bc77
Arek <t8534@o2.pl>
2017-01-05 23:31:58 +0100

    [freertos] Init.

---
+++ BlinkingLed/main_orig.c
@@ -0,0 +1,49 @@
+/*
+ * Base Core Atmega328
+ *
+ * Author : arek
+ *
+ * FreeRTOS
+ *
+ * http://www.freertos.org/a00098.html
+ * 
+ * http://www.freertos.org/a00090.html 
+ *
+ */ 
+
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "dbg.h"
+
+
+
+int main(void)
+{
+	const char *msg1 = "The value of val = ";	
+	const char *msg_crlf= "\r\n";	
+	uint32_t val = 0;
+
+	DBG_PINS_INIT;
+	DBG_UartInit()	;
+	while(1)
+	{
+		PORTB |= (1<<PORTB0);  // LED
+		DBG_PIN0_TOGGLE;
+		DBG_PIN1_TOGGLE;
+		DBG_PIN2_TOGGLE;
+		
+		_delay_ms(500);
+		
+		DBG_UartPrintStr(msg1);
+		DBG_UartPrintDec(DBG_DecMsgBuff, DBG_DEC_MSG_BUFF_LEN, val);
+		DBG_UartPrintStr(msg_crlf);
+		
+		PORTB &= ~(1<<PORTB0);
+		_delay_ms(500);
+		
+		if (val < 99999999) val++;
+	}
+	
+}
