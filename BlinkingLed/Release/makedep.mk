7081a47644fe57b44e4491b173f786179228a784
Arek <t8534@o2.pl>
2017-01-08 10:51:06 +0100

    [sh1106_oled_1_3] Compilable. Ready for tests. Bitmap displaying is todo, because of the PROGMEM usage to analyze.

--- BlinkingLed/Release/makedep.mk
+++ BlinkingLed/Release/makedep.mk
@@ -4,5 +4,11 @@
 
 dbg.c
 
+Fonts.c
+
 main.c
+
+SSD1306.c
+
+TWI_Master.c
 
--- BlinkingLed/main.c
+++ BlinkingLed/main.c
@@ -9,14 +9,65 @@
 #define F_CPU 8000000UL
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
+#include <stdint.h>
 #include "dbg.h"
+#include "TWI_Master.h"
+#include "Fonts.h"
+#include "SSD1306.h"
 
 
 
 int main(void)
 {
-	const char *msg1 = "The value of val = ";	
+	
+  // Init I2C	
+  TWI_Master_Initialise();
+  sei();	
+	
+  ssd1306_init();
+	
+	
+    ssd1306_clear_screen(0xFF);
+	_delay_ms(1000);
+	ssd1306_clear_screen(0x00);
+	ssd1306_display_string(18, 0, "1.3inch OLED", 16, 1);
+	ssd1306_display_string(0, 16, "This is a demo for SSD1306/1106 OLED moudle!", 16, 1);
+	ssd1306_refresh_gram();
+	_delay_ms(1000);
+	ssd1306_clear_screen(0x00);
+
+    /*
+	// todo: progmem problem
+	ssd1306_draw_bitmap(0, 2, &c_chSingal816[0], 16, 8);
+	ssd1306_draw_bitmap(24, 2, &c_chBluetooth88[0], 8, 8);
+	ssd1306_draw_bitmap(40, 2, &c_chMsg816[0], 16, 8);
+	ssd1306_draw_bitmap(64, 2, &c_chGPRS88[0], 8, 8);
+	ssd1306_draw_bitmap(90, 2, &c_chAlarm88[0], 8, 8);
+	ssd1306_draw_bitmap(112, 2, &c_chBat816[0], 16, 8);
+    */
+
+	ssd1306_draw_3216char(0,16, '2');
+	ssd1306_draw_3216char(16,16, '3');
+	ssd1306_draw_3216char(32,16, ':');
+	ssd1306_draw_3216char(48,16, '5');
+	ssd1306_draw_3216char(64,16, '6');
+	ssd1306_draw_1616char(80,32, ':');
+	ssd1306_draw_1616char(96,32, '4');
+	ssd1306_draw_1616char(112,32, '7');
+	//ssd1306_draw_bitmap(87, 16, &c_chBmp4016[0], 40, 16);  // todo: progmem problem
+
+	ssd1306_display_string(0, 52, "MUSIC", 12, 0);
+	ssd1306_display_string(52, 52, "MENU", 12, 0);
+	ssd1306_display_string(98, 52, "PHONE", 12, 0);
+	
+	ssd1306_refresh_gram();
+	while (1) {};	
+	
+	
+	
+/*	const char *msg1 = "The value of val = ";	
 	const char *msg_crlf= "\r\n";	
 	uint32_t val = 0;
 
@@ -40,5 +91,7 @@ int main(void)
 		
 		if (val < 99999999) val++;
 	}
+*/
+
 	
 }
